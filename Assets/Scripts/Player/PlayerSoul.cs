b130116e9c69d90c95b939191cbeb54cba0dfb7a
Vespukki <griffinctamm@gmail.com>
2023-03-15 22:35:38 -0500

    new player sprite and soul anim

--- Assets/Scripts/Player/PlayerSoul.cs
+++ Assets/Scripts/Player/PlayerSoul.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using Twosies.States;
+using Twosies.Utils;
 
 namespace Twosies.Player
 {
     [RequireComponent(typeof(PlayerInput))]
     public class PlayerSoul : MonoBehaviour
     {
+        private SpriteRenderer spriter;
+        private TrailRenderer trail;
         [HideInInspector] public PlayerInput input;
         public static PlayerSoul instance;
+        [SerializeField] private float soulTransferTime;
+
+        private Vector2 initialSoulPosition;
 
         private void Awake()
         {
@@ -24,13 +30,41 @@ namespace Twosies.Player
             }
 
             input = GetComponent<PlayerInput>();
+            spriter = GetComponent<SpriteRenderer>();
+            trail = GetComponent<TrailRenderer>();
+        }
+
+        private void Update()
+        {
+            //float t = Vector2.Distance(transform.position, transform.parent.position) / Vector2.Distance(transform.position, transform.parent.position);
+            Vector2 newPos = Vector2.MoveTowards(transform.position, transform.parent.position, .075f);
+
+            if (newPos == (Vector2)transform.parent.position)
+            {
+                SetVisual(false);
+            }
+            else
+            {
+                transform.position = newPos;
+            }
+        }
+
+        private void SetVisual(bool value)
+        {
+            spriter.enabled = value;
+            trail.emitting = value;
         }
 
         public static void TransferSoul(InputStateMachine prev, InputStateMachine target)
         {
-            prev.soul.transform.parent = target.transform;
+            PlayerSoul soul = prev.soul;
+            soul.SetVisual(true);
+
+            soul.initialSoulPosition = prev.transform.position;
+
+            soul.transform.parent = target.transform;
             prev.OnSoulExit();
-            target.OnSoulEnter(prev.soul);
+            target.OnSoulEnter(soul);
             prev.soul = null;
         }
     }
